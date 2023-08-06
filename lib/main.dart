c24818301add53bc50a3080235f8e8309106e186
AnTomS <thekeibee@yandex.ru>
2023-08-06 22:08:00 +0800

    логи Навигации, Блоков и Запросов через Talker

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:async';
+
 import 'package:dart_application_1/repositories/crypto_coins/crypto_coins.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:talker_bloc_logger/talker_bloc_logger.dart';
+import 'package:talker_dio_logger/talker_dio_logger.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
 import 'crypto_coins_list_app.dart';
@@ -11,7 +16,26 @@ void main() {
   GetIt.I.registerSingleton(talker);
   GetIt.I<Talker>().debug("Talker initialized");
 
+  Bloc.observer = TalkerBlocObserver(
+      talker: talker,
+      settings: const TalkerBlocLoggerSettings(
+        printEventFullData: false,
+        printStateFullData: false,
+      ));
+
+  final dio = Dio();
+  dio.interceptors.add(
+    TalkerDioLogger(
+      talker: talker,
+      settings: const TalkerDioLoggerSettings(
+        printResponseData: false,
+      ),
+    ),
+  );
+
   GetIt.I.registerLazySingleton<AbstactCoinsRepository>(
-      () => CryptoCoinsRepository(dio: Dio()));
-  runApp(const CryptoCurrenciesListApp());
+      () => CryptoCoinsRepository(dio: dio));
+
+  runZonedGuarded(() => runApp(const CryptoCurrenciesListApp()),
+      (error, stack) => GetIt.I<Talker>().handle(error, stack));
 }
